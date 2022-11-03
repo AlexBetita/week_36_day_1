4ca542a2d0ccc8694a27f89bd86e3ddd14e2a4fc
Daniel Wong <Daniel.kachun.wong@gmail.com>
2022-11-02 20:09:24 -0400

    fixed import cart route, added css reset file

--- app/routes/customers.py
+++ app/routes/customers.py
@@ -3,7 +3,8 @@ from ..models import Customer
 
 customers_bp = Blueprint("customers", __name__, url_prefix="/customers")
 
+
 @customers_bp.route('/', methods=['GET'])
 def display_all_customers():
-  customers = Customer.query.all()
-  return render_template('customers.html', customers=customers)
+    customers = Customer.query.order_by(Customer.first_name).all()
+    return render_template('customers.html', customers=customers)
